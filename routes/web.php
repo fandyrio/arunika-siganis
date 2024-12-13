3b78f566dfd5bcfa5f6749e352f897bc964db508
Septiano Praditya Hartono <septianopraditya19@gmail.com>
2024-12-13 15:00:27 +0700

    fix
    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+Route::get('/', 'arunikaController@index');
 
 Route::get('register', 'registerController@index')->name('register')->middleware('isNotLogin');
 Route::post('validate-input', 'registerController@validateNIP')->name('validate-nip')->middleware('isNotLogin');
